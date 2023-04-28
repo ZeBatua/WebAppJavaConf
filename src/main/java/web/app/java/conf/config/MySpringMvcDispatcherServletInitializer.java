de7d40813a3c999894601a4e232b21b74bcb44ff
thalassa_home <blinov.gosha@mail.ru>
2023-04-28 20:02:33 +0300

    Initial commit

---
+++ src/main/java/web/app/java/conf/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,23 @@
+package web.app.java.conf.config;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
